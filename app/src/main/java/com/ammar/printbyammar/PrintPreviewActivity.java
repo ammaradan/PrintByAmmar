80410c1d3a92cc342f7eeda3f6b027830ff07314
ammaradan <ammar.sadiq.pk@gmail.com>
2026-04-29 11:50:25 +0500

    Fix app/src/main/java/com/ammar/printbyammar/PrintPreviewActivity.java

---
+++ app/src/main/java/com/ammar/printbyammar/PrintPreviewActivity.java
@@ -0,0 +1,15 @@
+package com.ammar.printbyammar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Print Preview Activity - stub for future implementation
+ */
+public class PrintPreviewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish(); // Close immediately, handled by MainActivity
+    }
+}
